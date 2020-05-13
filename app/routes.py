0879212488d69948fb9f616c0c3ed569f0538d99
Evaristo Ferreira <evaristofm@gmail.com>
2020-05-13 16:34:59 -0300

    Definindo o template base do projeto

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Evaristo'}
+    posts = [
+        {
+            'author': {'username': 'Jhon'},
+            'body': 'The Avangers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Beautiful day in Portland!'
+        }
+    ]
+    return render_template('index.html',title='Home', user=user, posts=posts)
